b8e4a2d02e22728782409f7bf99c33bdd402d2fb
Adrien Fallou <adrien.fallou@gmail.com>
2016-07-22 14:54:36 -0700

    Get location of files back from S3

--- server/projects/projects.controller.js
+++ server/projects/projects.controller.js
@@ -8,18 +8,41 @@ module.exports = class ProjectsController{
     this.model = new ProjectsModel();
   }
 
+  /**
+   * Create project, including the correct files and title
+   * @param req
+   * @param res
+   */
   create(req, res){
     var fstream;
     var that = this;
+
+    var projectProperties = {
+      fileLocations: {}
+    };
+
     // Upload file to file system before doing more
     req.busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
       that.model.uploadFile(file, filename)
         .then(filepath => {
           that.model.processUploadFile(filepath, filename)
+        })
+        .then(fileLocations => {
+          projectProperties.fileLocations = {
+            original: fileLocations[0],
+            thumb: fileLocations[1],
+            large: fileLocations[2]
+          }
         });
     });
+
+    req.busboy.on('field', (key, value, keyTruncated, valueTruncated) => {
+      projectProperties[key] = value;
+    });
+
     req.pipe(req.busboy);
 
+    this.model.createProject(projectProperties);
     res.send('Got request');
   }
 };
--- server/projects/projects.model.js
+++ server/projects/projects.model.js
@@ -12,6 +12,10 @@ module.exports = class ProjectsModel{
   constructor(){
   }
 
+  createProject(properties){
+
+  }
+
   /**
    * Upload file to local filesystem
    * @param {FileStream} file
@@ -31,22 +35,35 @@ module.exports = class ProjectsModel{
     });
   }
 
+  /**
+   * From the original file, build large version and thumbnails
+   * @param {String} filepath - Path of original file on local filesystem
+   * @param {String} filename - Name of file uploaded by user
+   * @returns {Promise}
+   */
   processUploadFile(filepath, filename){
-    this.uploadToS3(filepath, filename);
-
+    var promOrig = this.uploadToS3(filepath, filename);
     var size;
 
     var thumbPath = path.join(__dirname, '..', `tmp/${filename}_thumb.png`);
-    var largePath = path.join(__dirname, '..', `tmp/${filename}_large.png`)
+    var largePath = path.join(__dirname, '..', `tmp/${filename}_large.png`);
     // 'resize' will fit the image into the given dimensions
 
     this.squareResizeAndWrite(filepath, thumbPath, config.thumbPxSize);
     this.squareResizeAndWrite(filepath, largePath, config.largePxSize);
 
-    this.uploadToS3(thumbPath, `${filename}_thumb`);
-    this.uploadToS3(largePath, `${filename}_large`);
+    var promThumb = this.uploadToS3(thumbPath, `${filename}_thumb`);
+    var promLarge = this.uploadToS3(largePath, `${filename}_large`);
+
+    return Promise.all([promOrig, promThumb, promLarge]);
   }
 
+  /**
+   * Take file at path, resize into correct dimensions and write back to local filesystem
+   * @param {string} inPath
+   * @param {string} outPath
+   * @param {number} size
+   */
   squareResizeAndWrite(inPath, outPath, size){
     // Resize fits the image into the given dimension, while keeping aspect ratio
     gm(inPath)
@@ -58,16 +75,27 @@ module.exports = class ProjectsModel{
       });
   }
 
+  /**
+   * Upload file to S3
+   * @param {String} filepath - Path on local filesystem
+   * @param {String} filename - Name of file, that will be used as key on S3
+   * @returns {Promise}
+   */
   uploadToS3(filepath, filename){
     var outStream = fs.createReadStream(filepath);
     var s3Obj = new AWS.S3({params: {Bucket: config.s3Bucket, Key: filename}});
-    s3Obj.upload({Body: outStream})
-      .send((err, data) => {
-        if(err){
-          console.error(`Error uploading file ${filename} to S3: ${err}`);
-        } else {
-          console.log(`Successfully upload file ${filename} to S3`);
-        }
-      });
+
+    return new Promise((resolve, reject) => {
+      s3Obj.upload({Body: outStream})
+        .send((err, data) => {
+          if(err){
+            console.error(`Error uploading file ${filename} to S3: ${err}`);
+            reject();
+          } else {
+            console.log(`Successfully upload file ${filename} to S3`);
+            resolve(data.location);
+          }
+        });
+    });
   }
 };
